4682cb5a180baa4eee5630a63575a06ba7a1edb8
Balázs Csaba <csaba@balazs-magyar.hu>
2024-01-11 18:40:38 +0100

    fix: build process

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,8 @@ export default defineConfig({
 		minify: true,
 		cssMinify: true,
 		emptyOutDir: true,
-		outDir: 'docs'
+		outDir: 'docs',
+		chunkSizeWarningLimit: 1500
 	},
 	base: process.env.NODE_ENV === 'production' ? '/pcb-tht-holder' : '',
 	resolve: {

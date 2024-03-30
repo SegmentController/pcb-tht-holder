7e2cafa500d52d93c9e076ba1b3579cc3e704f7f
Balázs Csaba <csaba@balazs-magyar.hu>
2024-03-30 21:07:16 +0100

    v1.4.4

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,7 @@ const GHPATH = 'https://segmentcontroller.github.io/pcb-tht-holder/';
 
 const APP_PREFIX = 'pwa_pcb-tht-holder_';
 
-const VERSION = 'v1.4.3';
+const VERSION = 'v1.4.4';
 
 // The files to make available for offline use. make sure to add others to this list
 const URLS = [
